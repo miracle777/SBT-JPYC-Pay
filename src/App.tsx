cc94cd9db01413330ad17500dd74aa21380e57c6
Masaru Kumazaki <miracle777@users.noreply.github.com>
2025-11-14 01:18:45 +0900

    refactor: Remove unnecessary Router wrapper from App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
 import QRPayment from './pages/QRPayment';
 import SBTManagement from './pages/SBTManagement';
@@ -7,17 +7,15 @@ import NotFound from './pages/NotFound';
 
 function App() {
   return (
-    <Router>
-      <div className="App min-h-screen bg-gray-50">
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/payment" element={<QRPayment />} />
-          <Route path="/sbt" element={<SBTManagement />} />
-          <Route path="/settings" element={<Settings />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </div>
-    </Router>
+    <div className="App min-h-screen bg-gray-50">
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/payment" element={<QRPayment />} />
+        <Route path="/sbt" element={<SBTManagement />} />
+        <Route path="/settings" element={<Settings />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </div>
   );
 }
 
